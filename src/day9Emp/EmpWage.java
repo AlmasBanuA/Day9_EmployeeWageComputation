0623b3bf1da67e837ea9346f29b332508fae5527
[AlmasBanuA2011] <[almasbanu3304@gmail.com]>
2022-02-22 23:06:59 +0530

    calculating daily Employee wage

--- src/day9Emp/EmpWage.java
+++ src/day9Emp/EmpWage.java
@@ -1,21 +1,41 @@
 package day9Emp;
 
 /*
- * checking employee is present or not
+ * Check Employee is Present or Absent
  */
-import java.util.Random; //creating a random class
+
+// import random class
+import java.util.Random;
 
 class EmpWageComputation {
 	Random rand = new Random();
 	public int isPresent = 1;
 	public int isAbsent = 2;
+	public int wagePerHr = 20;
+	public int fullDayHr = 8;
+	int check = rand.nextInt(2) + 1;
 
-	public void present() {
-		int check = rand.nextInt(2) + 1;
+	/*
+	 * boolean method to return true or false
+	 */
+	public boolean present() {
 		if (check == isAbsent) {
 			System.out.println("Employee is absent.");
+			return false;
 		} else {
 			System.out.println("Employee is present.");
+			return true;
+		}
+	}
+
+	/*
+	 * Calculating Daily Employee Wage
+	 */
+
+	public void dailyWage() {
+		if (present()) {
+			int dailyWage = wagePerHr * fullDayHr;
+			System.out.println("Wage of the day of employee is : " + dailyWage);
 		}
 	}
 }
@@ -24,12 +44,9 @@ public class EmpWage {
 
 	public static void main(String[] args) {
 		System.out.println("Welcome To Employee Wage Computation");
-
-		/*
-		 * creating empWage object
-		 */
 		EmpWageComputation empWage = new EmpWageComputation();
 		empWage.present();
+		empWage.dailyWage();
 	}
 
 }
