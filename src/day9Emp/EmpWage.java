6cd3e073b694b6c4b0611524678dbe0d3eb4a536
AlmasBanuA <94681887+AlmasBanuA@users.noreply.github.com>
2022-02-23 20:03:24 +0530

    checking employee is present or not

--- src/day9Emp/EmpWage.java
+++ src/day9Emp/EmpWage.java
@@ -3,33 +3,15 @@ package day9Emp;
 /*
  * checking employee is present or not
  */
-import java.util.Random; //creating a random class
-
-class EmpWageComputation {
-	Random rand = new Random();
-	public int isPresent = 1;
-	public int isAbsent = 2;
-
-	public void present() {
-		int check = rand.nextInt(2) + 1;
-		if (check == isAbsent) {
-			System.out.println("Employee is absent.");
-		} else {
-			System.out.println("Employee is present.");
-		}
-	}
-}
-
-public class EmpWage {
-
-	public static void main(String[] args) {
-		System.out.println("Welcome To Employee Wage Computation");
-
-		/*
-		 * creating empWage object
-		 */
-		EmpWageComputation empWage = new EmpWageComputation();
-		empWage.present();
-	}
-
+public class Employee_wage {
+	    public static void main(String[] args)
+	    {
+	        int empCheck = (int)(Math.floor(Math.random()*10)%2);
+	        if ( empCheck == 1 ){
+	            System.out.println("Employee is Present");
+	        }
+	        else{
+	            System.out.println("Employee is Absent");
+	        }
+  }
 }
