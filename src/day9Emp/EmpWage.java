0471cd999330b8b63eb6b366f338c49d3b07f0af
[AlmasBanuA2011] <[almasbanu3304@gmail.com]>
2022-02-23 20:31:17 +0530

    Calculating wages for a month

--- src/day9Emp/EmpWage.java
+++ src/day9Emp/EmpWage.java
@@ -1,48 +1,35 @@
 package day9Emp;
 
-import java.util.Random;
-
 /*
-    Program solving Using Switch case statement
+    Calculating wages for a month
 */
 
 class EmpWageComputation {
-	Random rand = new Random();
-	public final int isPresent = 1;
-	public final int isAbsent = 2;
-	public final int isPartTime = 3;
-	public int wagePerHr = 20;
-	public int fullDayHr = 8;
-	public int partTimeHr = 4;
-	public int dailyWage;
 
-	public void present() {
-		int check = rand.nextInt(3) + 1;
-		switch (check) {
-		case isPresent: {
-			System.out.println("Employee is present.");
-			dailyWage = wagePerHr * fullDayHr; // condition 1
-			System.out.println("Daily wage is : " + dailyWage);
-		}
-
-		case isPartTime: {
-			System.out.println("Employee is present part time."); // condition 2
-			dailyWage = wagePerHr * partTimeHr;
-			System.out.println("Daily wage is : " + dailyWage);
-		}
-
-		case isAbsent:
-			System.out.println("Employee is absent."); // default statement
-		}
-	}
-}
-
-public class EmpWage {
+	public static final int EMP_FULL_TIME_HOUR = 1;
+	public static final int EMP_PART_TIME_HOUR = 2;
+	public static final int EMP_WAGE_PER_HOUR = 20;
+	public static final int EMP_WORKING_DAY_MONTH = 20;
 
 	public static void main(String[] args) {
-		System.out.println("Welcome To Employee Wage Computation");
-		EmpWageComputation empWage = new EmpWageComputation(); // create object for EmpWageComputation class
-		empWage.present();
+		int empHours;
+		int empCheck = (int) (Math.floor(Math.random() * 10) % 3);
+		switch (empCheck) {
+		case EMP_FULL_TIME_HOUR -> {
+			empHours = 8;
+			System.out.println("Employee is present for full time");
+		}
+		case EMP_PART_TIME_HOUR -> {
+			empHours = 4;
+			System.out.println("Employee is present for half time");
+		}
+		default -> {
+			empHours = 0;
+			System.out.println("Employee is absent");
+		}
+		}
+		int empWage = (EMP_WAGE_PER_HOUR * empHours);
+		int totalEmpWage = (empWage * EMP_WORKING_DAY_MONTH);
+		System.out.println("Employee Wage = " + totalEmpWage);
 	}
-
 }
